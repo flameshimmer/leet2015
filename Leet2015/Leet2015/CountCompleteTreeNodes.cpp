41799afd89f8bcbc07018ce558034b60b4f65641
flameshimmer <nm1072@gmail.com>
2015-06-22 11:06:24 -0700

    CountCompleteTreeNode

--- Leet2015/Leet2015/CountCompleteTreeNodes.cpp
+++ Leet2015/Leet2015/CountCompleteTreeNodes.cpp
@@ -5,48 +5,36 @@
 //Definition of a complete binary tree from Wikipedia :
 //In a complete binary tree every level, except possibly the last, is completely filled, 
 //and all nodes in the last level are as far left as possible.It can have between 1 and 2h nodes inclusive at the last level h.
+/*
+		    1
+		 /     \
+	    2       3
+	  /   \    / \
+	 4     5  6   7
+	/ \   / 
+   8   9 10  
 
+*/
 namespace Solution1
-{
-	void countNodes(TreeNode* node, int curHeight, int& maxHeight, int& maxHeightCount, int& extra)
-	{
-		if (!node->left && !node->right)
-		{
-			if (maxHeight < 0)
-			{
-				maxHeight = curHeight;
-			}
-		}
-		if (curHeight == maxHeight)
-		{
-			if (!node->left && !node->right)
-			{
-				maxHeightCount++;
-			}
-			else
-			{
-				if (node->left) { extra++; }
-				if (node->right) { extra++; }
-				return;
-			}
-		}
-		else if (curHeight < maxHeight)
-		{
-			countNodes(node->right, curHeight+1, maxHeight, maxHeightCount, extra);
-			countNodes(node->left, curHeight + 1, maxHeight, maxHeightCount, extra);
-		}
-	}
-
+{	
 	int countNodes(TreeNode* root) {
 		if (!root) { return 0; }
+		int h = 1;
+		TreeNode* left = root->left;
+		TreeNode* right = root->right;
+		while (left && right)
+		{
+			h++;
+			left = left->left;
+			right = right->right;
+		}
+		 
+		if (left == right) 
+		{ 
+			return (1 << h) -1; 
+		}
 
-		int count = 1;
-		int maxHeight = 1;
-		int maxHeightCount = 1;
-		int extra = 0;
-		countNodes(root->right, 1, maxHeight, maxHeightCount, extra);
-		countNodes(root->left, 1, maxHeight, maxHeightCount, extra);
-		return count;
+		return countNodes(root->left) + countNodes(root->right) + 1;
 	}
 
 	void CountCompleteTreeNodes()
